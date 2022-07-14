6e3ba4a8ed7e3271e33cc63f905ebfb6cc0a0d59
Mattew Diniz <mattew.diniz@m2tw.com.br>
2022-07-13 21:49:06 -0400

    Initial Commit

---
+++ 01-foo/src/main/java/br/com/m2tw/foo/StartupApp.java
@@ -0,0 +1,13 @@
+package br.com.m2tw.foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupApp.class, args);
+	}
+
+}
---
+++ 01-foo/src/test/java/br/com/m2tw/foo/StartupAppTests.java
@@ -0,0 +1,13 @@
+package br.com.m2tw.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
